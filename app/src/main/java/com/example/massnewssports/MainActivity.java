c067d07d88db88302f10935524498bcb774e5d10
Biggie <ahmed_mikky@bloomfield.edu>
2022-03-31 19:53:23 -0400

    check

--- app/src/main/java/com/example/massnewssports/MainActivity.java
+++ app/src/main/java/com/example/massnewssports/MainActivity.java
@@ -1,18 +1,11 @@
 package com.example.massnewssports;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.massnewssports.activities.LiveActivity;
-import com.example.massnewssports.adapters.LiveAdapter;
-import com.example.massnewssports.models.LiveScore;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.massnewssports.fragments.LiveScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Intent i = new Intent(this, LiveActivity.class);
+        Intent i = new Intent(this, LiveScoreFragment.class);
         startActivity(i);
     }
 }
